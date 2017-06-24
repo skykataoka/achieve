f10b3028d3a0b21c3e021fd2f4b9cef9db5ad247
KataokaSusumu <skykataoka@gmail.com>
2017-06-24 14:30:01 +0900

    rails g時の不要ファイル生成防止

--- spec/models/contacts_spec.rb
+++ spec/models/contacts_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 describe Contact do
-  # タイトルと内容があれば有効な状態であること
+
   it "タイトル, email, 内容が入っていれば妥当と判断" do
     contact = Contact.new(title: '宮岡', email: 'skykataoka@dic.com', content: '暑いです')
     expect(contact).to be_valid
   end
 
-  #タイトルがなければ無効であること
   it "タイトル, email, 内容が入っていなければ無効と判断" do
     contact = Contact.new
     expect(contact).not_to be_valid
   end
 
-  #タイトルがなければ無効であること
   it "タイトル, email, 内容が入っていない時エラーが表示されていれば妥当と判断" do
     contact = Contact.new
     contact.valid?
